21f80b744f82a9eb2e3f3ce9d8d40c0c72108f41
kdeldycke <159718+kdeldycke@users.noreply.github.com>
2024-12-02 10:36:34 +0000

    [autofix] Format Python

--- tests/test_platform_data.py
+++ tests/test_platform_data.py
@@ -23,55 +23,55 @@ from extra_platforms import ALL_OS_LABELS, ALL_PLATFORMS
 import pytest
 import requests
 
+
 @pytest.mark.parametrize("platform", ALL_PLATFORMS.platforms)
 def test_platform_definitions(platform):
+    assert platform
 
-        assert platform
+    # ID.
+    assert platform.id
+    assert platform.id.isascii()
+    assert platform.id[0] in ascii_lowercase
+    assert platform.id[-1] in ascii_lowercase + digits
+    assert set(platform.id).issubset(ascii_lowercase + digits + "_")
+    assert platform.id.islower()
+    # Platforms are not allowed to starts with all_ or any_, which is reserved
+    # for groups. Use unknown_ prefix instead.
+    assert not platform.id.startswith(("all_", "any_"))
 
-        # ID.
-        assert platform.id
-        assert platform.id.isascii()
-        assert platform.id[0] in ascii_lowercase
-        assert platform.id[-1] in ascii_lowercase + digits
-        assert set(platform.id).issubset(ascii_lowercase + digits + "_")
-        assert platform.id.islower()
-        # Platforms are not allowed to starts with all_ or any_, which is reserved
-        # for groups. Use unknown_ prefix instead.
-        assert not platform.id.startswith(("all_", "any_"))
+    # Name.
+    assert platform.name
+    assert platform.name.isascii()
+    assert platform.name.isprintable()
+    assert platform.name in ALL_OS_LABELS
 
-        # Name.
-        assert platform.name
-        assert platform.name.isascii()
-        assert platform.name.isprintable()
-        assert platform.name in ALL_OS_LABELS
+    # Icon.
+    assert platform.icon
+    assert 2 >= len(platform.icon) >= 1
 
-        # Icon.
-        assert platform.icon
-        assert 2 >= len(platform.icon) >= 1
+    # URL.
+    assert platform.url
+    assert platform.url.startswith("https://")
+    if platform.id == "raspbian":
+        pytest.xfail("raspberrypi.com is blocking access from GitHub Actions")
+    with requests.get(platform.url) as response:
+        assert response.ok, f"{platform.url} is not reachable: {response}"
 
-        # URL.
-        assert platform.url
-        assert platform.url.startswith("https://")
-        if platform.id == "raspbian":
-            pytest.xfail("raspberrypi.com is blocking access from GitHub Actions")
-        with requests.get(platform.url) as response:
-            assert response.ok, f"{platform.url} is not reachable: {response}"
-
-        # Info.
-        assert platform.info()
-        for k, v in platform.info().items():
-            assert set(k).issubset(ascii_lowercase + "_")
-            if v is not None:
-                assert isinstance(v, (str, bool, dict))
-                if isinstance(v, str):
-                    assert v
-                elif isinstance(v, dict):
-                    assert v
-                    for k1, v1 in v.items():
-                        assert set(k1).issubset(ascii_lowercase + "_")
-                        if v1 is not None:
-                            assert v1
-        assert platform.info()["id"] == platform.id
+    # Info.
+    assert platform.info()
+    for k, v in platform.info().items():
+        assert set(k).issubset(ascii_lowercase + "_")
+        if v is not None:
+            assert isinstance(v, (str, bool, dict))
+            if isinstance(v, str):
+                assert v
+            elif isinstance(v, dict):
+                assert v
+                for k1, v1 in v.items():
+                    assert set(k1).issubset(ascii_lowercase + "_")
+                    if v1 is not None:
+                        assert v1
+    assert platform.info()["id"] == platform.id
 
 
 def test_os_labels():
