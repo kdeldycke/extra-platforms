25549fa988b81053c106e4eec4205513d813a4ab
Kevin Deldycke <kevin@deldycke.com>
2025-10-21 16:24:27 +0400

    Format

--- extra_platforms/detection.py
+++ extra_platforms/detection.py
@@ -82,25 +82,25 @@ from . import _report_msg
 
 @cache
 def is_aix() -> bool:
-    """Return `True` if current platform is AIX."""
+    """Return ``True`` if current platform is AIX."""
     return sys.platform.startswith("aix") or distro.id() == "aix"
 
 
 @cache
 def is_altlinux() -> bool:
-    """Return `True` if current platform is ALT Linux."""
+    """Return ``True`` if current platform is ALT Linux."""
     return distro.id() == "altlinux"
 
 
 @cache
 def is_amzn() -> bool:
-    """Return `True` if current platform is Amazon Linux."""
+    """Return ``True`` if current platform is Amazon Linux."""
     return distro.id() == "amzn"
 
 
 @cache
 def is_android() -> bool:
-    """Return `True` if current platform is Android.
+    """Return ``True`` if current platform is Android.
 
     Source: https://github.com/kivy/kivy/blob/master/kivy/utils.py#L429
     """
@@ -109,13 +109,13 @@ def is_android() -> bool:
 
 @cache
 def is_arch() -> bool:
-    """Return `True` if current platform is Arch Linux."""
+    """Return ``True`` if current platform is Arch Linux."""
     return distro.id() == "arch"
 
 
 @cache
 def is_azure_pipelines() -> bool:
-    """Return `True` if current platform is Azure Pipelines.
+    """Return ``True`` if current platform is Azure Pipelines.
 
     `Environment variables reference
     <https://learn.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&viewFallbackFrom=vsts&tabs=yaml#system-variables>`_.
@@ -125,7 +125,7 @@ def is_azure_pipelines() -> bool:
 
 @cache
 def is_bamboo() -> bool:
-    """Return `True` if current platform is Bamboo.
+    """Return ``True`` if current platform is Bamboo.
 
     `Environment variables reference
     <https://confluence.atlassian.com/bamboo/bamboo-variables-289277087.html#Bamboovariables-Build-specificvariables>`_.
@@ -135,7 +135,7 @@ def is_bamboo() -> bool:
 
 @cache
 def is_buildkite() -> bool:
-    """Return `True` if current platform is Buildkite.
+    """Return ``True`` if current platform is Buildkite.
 
     `Environment variables reference
     <https://buildkite.com/docs/pipelines/environment-variables>`_.
@@ -145,19 +145,19 @@ def is_buildkite() -> bool:
 
 @cache
 def is_buildroot() -> bool:
-    """Return `True` if current platform is Buildroot."""
+    """Return ``True`` if current platform is Buildroot."""
     return distro.id() == "buildroot"
 
 
 @cache
 def is_centos() -> bool:
-    """Return `True` if current platform is CentOS."""
+    """Return ``True`` if current platform is CentOS."""
     return distro.id() == "centos"
 
 
 @cache
 def is_circle_ci() -> bool:
-    """Return `True` if current platform is Circle CI.
+    """Return ``True`` if current platform is Circle CI.
 
     `Environment variables reference
     <https://circleci.com/docs/2.0/env-vars/#built-in-environment-variables>`_.
@@ -167,7 +167,7 @@ def is_circle_ci() -> bool:
 
 @cache
 def is_cirrus_ci() -> bool:
-    """Return `True` if current platform is Cirrus CI.
+    """Return ``True`` if current platform is Cirrus CI.
 
     `Environment variables reference
     <https://cirrus-ci.org/guide/writing-tasks/#environment-variables>`_.
@@ -177,13 +177,13 @@ def is_cirrus_ci() -> bool:
 
 @cache
 def is_cloudlinux() -> bool:
-    """Return `True` if current platform is CloudLinux OS."""
+    """Return ``True`` if current platform is CloudLinux OS."""
     return distro.id() == "cloudlinux"
 
 
 @cache
 def is_codebuild() -> bool:
-    """Return `True` if current platform is CodeBuild.
+    """Return ``True`` if current platform is CodeBuild.
 
     `Environment variables reference
     <https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-env-vars.html>`_.
@@ -193,43 +193,43 @@ def is_codebuild() -> bool:
 
 @cache
 def is_cygwin() -> bool:
-    """Return `True` if current platform is Cygwin."""
+    """Return ``True`` if current platform is Cygwin."""
     return sys.platform.startswith("cygwin")
 
 
 @cache
 def is_debian() -> bool:
-    """Return `True` if current platform is Debian."""
+    """Return ``True`` if current platform is Debian."""
     return distro.id() == "debian"
 
 
 @cache
 def is_exherbo() -> bool:
-    """Return `True` if current platform is Exherbo Linux."""
+    """Return ``True`` if current platform is Exherbo Linux."""
     return distro.id() == "exherbo"
 
 
 @cache
 def is_fedora() -> bool:
-    """Return `True` if current platform is Fedora."""
+    """Return ``True`` if current platform is Fedora."""
     return distro.id() == "fedora"
 
 
 @cache
 def is_freebsd() -> bool:
-    """Return `True` if current platform is FreeBSD."""
+    """Return ``True`` if current platform is FreeBSD."""
     return sys.platform.startswith("freebsd") or distro.id() == "freebsd"
 
 
 @cache
 def is_gentoo() -> bool:
-    """Return `True` if current platform is GenToo Linux."""
+    """Return ``True`` if current platform is GenToo Linux."""
     return distro.id() == "gentoo"
 
 
 @cache
 def is_github_ci() -> bool:
-    """Return `True` if current platform is GitHub Actions runner.
+    """Return ``True`` if current platform is GitHub Actions runner.
 
     `Environment variables reference
     <https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables>`_.
@@ -239,23 +239,23 @@ def is_github_ci() -> bool:
 
 @cache
 def is_gitlab_ci() -> bool:
-    """Return `True` if current platform is GitLab CI.
+    """Return ``True`` if current platform is GitLab CI.
 
     `Environment variables reference
-    https://docs.gitlab.com/ci/variables/predefined_variables/#predefined-variables>`_.
+    <https://docs.gitlab.com/ci/variables/predefined_variables/#predefined-variables>`_.
     """
     return "GITLAB_CI" in environ
 
 
 @cache
 def is_guix() -> bool:
-    """Return `True` if current platform is Guix System."""
+    """Return ``True`` if current platform is Guix System."""
     return distro.id() == "guix"
 
 
 @cache
 def is_heroku_ci() -> bool:
-    """Return `True` if current platform is Heroku CI.
+    """Return ``True`` if current platform is Heroku CI.
 
     `Environment variables reference
     <https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables>`_.
@@ -265,9 +265,9 @@ def is_heroku_ci() -> bool:
 
 @cache
 def is_hurd() -> bool:
-    """Return `True` if current platform is GNU/Hurd.
+    """Return ``True`` if current platform is GNU/Hurd.
 
-    `sys.platform` can returns `GNU` or `gnu0`, see:
+    ``sys.platform`` can returns ``GNU`` or ``gnu0``, see:
     https://github.com/kdeldycke/extra-platforms/issues/308
     """
     return sys.platform.lower().startswith("gnu")
@@ -275,139 +275,139 @@ def is_hurd() -> bool:
 
 @cache
 def is_ibm_powerkvm() -> bool:
-    """Return `True` if current platform is IBM PowerKVM."""
+    """Return ``True`` if current platform is IBM PowerKVM."""
     return distro.id() == "ibm_powerkvm"
 
 
 @cache
 def is_kvmibm() -> bool:
-    """Return `True` if current platform is KVM for IBM z Systems."""
+    """Return ``True`` if current platform is KVM for IBM z Systems."""
     return distro.id() == "kvmibm"
 
 
 @cache
 def is_linuxmint() -> bool:
-    """Return `True` if current platform is Linux Mint."""
+    """Return ``True`` if current platform is Linux Mint."""
     return distro.id() == "linuxmint"
 
 
 @cache
 def is_macos() -> bool:
-    """Return `True` if current platform is macOS."""
+    """Return ``True`` if current platform is macOS."""
     return platform.platform(terse=True).startswith(("macOS", "Darwin"))
 
 
 @cache
 def is_mageia() -> bool:
-    """Return `True` if current platform is Mageia."""
+    """Return ``True`` if current platform is Mageia."""
     return distro.id() == "mageia"
 
 
 @cache
 def is_mandriva() -> bool:
-    """Return `True` if current platform is Mandriva Linux."""
+    """Return ``True`` if current platform is Mandriva Linux."""
     return distro.id() == "mandriva"
 
 
 @cache
 def is_midnightbsd() -> bool:
-    """Return `True` if current platform is MidnightBSD."""
+    """Return ``True`` if current platform is MidnightBSD."""
     return sys.platform.startswith("midnightbsd") or distro.id() == "midnightbsd"
 
 
 @cache
 def is_netbsd() -> bool:
-    """Return `True` if current platform is NetBSD."""
+    """Return ``True`` if current platform is NetBSD."""
     return sys.platform.startswith("netbsd") or distro.id() == "netbsd"
 
 
 @cache
 def is_nobara() -> bool:
-    """Return `True` if current platform is Nobara Linux."""
+    """Return ``True`` if current platform is Nobara Linux."""
     return distro.id() == "nobara"
 
 
 @cache
 def is_openbsd() -> bool:
-    """Return `True` if current platform is OpenBSD."""
+    """Return ``True`` if current platform is OpenBSD."""
     return sys.platform.startswith("openbsd") or distro.id() == "openbsd"
 
 
 @cache
 def is_opensuse() -> bool:
-    """Return `True` if current platform is openSUSE."""
+    """Return ``True`` if current platform is openSUSE."""
     return distro.id() == "opensuse"
 
 
 @cache
 def is_oracle() -> bool:
-    """Return `True` if current platform is Oracle Linux (and Oracle Enterprise Linux)."""
+    """Return ``True`` if current platform is Oracle Linux (and Oracle Enterprise Linux)."""
     return distro.id() == "oracle"
 
 
 @cache
 def is_parallels() -> bool:
-    """Return `True` if current platform is Parallels."""
+    """Return ``True`` if current platform is Parallels."""
     return distro.id() == "parallels"
 
 
 @cache
 def is_pidora() -> bool:
-    """Return `True` if current platform is Pidora."""
+    """Return ``True`` if current platform is Pidora."""
     return distro.id() == "pidora"
 
 
 @cache
 def is_raspbian() -> bool:
-    """Return `True` if current platform is Raspbian."""
+    """Return ``True`` if current platform is Raspbian."""
     return distro.id() == "raspbian"
 
 
 @cache
 def is_rhel() -> bool:
-    """Return `True` if current platform is RedHat Enterprise Linux."""
+    """Return ``True`` if current platform is RedHat Enterprise Linux."""
     return distro.id() == "rhel"
 
 
 @cache
 def is_rocky() -> bool:
-    """Return `True` if current platform is Rocky Linux."""
+    """Return ``True`` if current platform is Rocky Linux."""
     return distro.id() == "rocky"
 
 
 @cache
 def is_scientific() -> bool:
-    """Return `True` if current platform is Scientific Linux."""
+    """Return ``True`` if current platform is Scientific Linux."""
     return distro.id() == "scientific"
 
 
 @cache
 def is_slackware() -> bool:
-    """Return `True` if current platform is Slackware."""
+    """Return ``True`` if current platform is Slackware."""
     return distro.id() == "slackware"
 
 
 @cache
 def is_sles() -> bool:
-    """Return `True` if current platform is SUSE Linux Enterprise Server."""
+    """Return ``True`` if current platform is SUSE Linux Enterprise Server."""
     return distro.id() == "sles"
 
 
 @cache
 def is_solaris() -> bool:
-    """Return `True` if current platform is Solaris."""
+    """Return ``True`` if current platform is Solaris."""
     return platform.platform(aliased=True, terse=True).startswith("Solaris")
 
 
 @cache
 def is_sunos() -> bool:
-    """Return `True` if current platform is SunOS."""
+    """Return ``True`` if current platform is SunOS."""
     return platform.platform(aliased=True, terse=True).startswith("SunOS")
 
 
 @cache
 def is_teamcity() -> bool:
-    """Return `True` if current platform is TeamCity.
+    """Return ``True`` if current platform is TeamCity.
 
     `Environment variables reference
     <https://www.jetbrains.com/help/teamcity/predefined-build-parameters.html#PredefinedBuildParameters-ServerBuildProperties>`_.
@@ -417,7 +417,7 @@ def is_teamcity() -> bool:
 
 @cache
 def is_travis_ci() -> bool:
-    """Return `True` if current platform is Travis CI.
+    """Return ``True`` if current platform is Travis CI.
 
     `Environment variables reference
     <https://docs.travis-ci.com/user/environment-variables/#default-environment-variables>`_.
@@ -427,31 +427,31 @@ def is_travis_ci() -> bool:
 
 @cache
 def is_tumbleweed() -> bool:
-    """Return `True` if current platform is openSUSE Tumbleweed."""
+    """Return ``True`` if current platform is openSUSE Tumbleweed."""
     return distro.id() == "opensuse-tumbleweed"
 
 
 @cache
 def is_tuxedo() -> bool:
-    """Return `True` if current platform is Tuxedo OS."""
+    """Return ``True`` if current platform is Tuxedo OS."""
     return distro.id() == "tuxedo"
 
 
 @cache
 def is_ubuntu() -> bool:
-    """Return `True` if current platform is Ubuntu."""
+    """Return ``True`` if current platform is Ubuntu."""
     return distro.id() == "ubuntu"
 
 
 @cache
 def is_unknown_ci() -> bool:
-    """Return `True` if current platform is an unknown CI.
+    """Return ``True`` if current platform is an unknown CI.
 
     Some CI systems relies on `generic environment variables to identify themselves
     <https://adamj.eu/tech/2020/03/09/detect-if-your-tests-are-running-on-ci/>`_:
 
-        - `CI`
-        - `BUILD_ID`
+    - ``CI``
+    - ``BUILD_ID``
     """
     if any((
         is_azure_pipelines(),
@@ -476,7 +476,7 @@ def is_unknown_ci() -> bool:
 
 @cache
 def is_unknown_linux() -> bool:
-    """Return `True` if current platform is an unknown Linux."""
+    """Return ``True`` if current platform is an unknown Linux."""
     if any((
         is_altlinux(),
         is_amzn(),
@@ -521,13 +521,13 @@ def is_unknown_linux() -> bool:
 
 @cache
 def is_windows() -> bool:
-    """Return `True` if current platform is Windows."""
+    """Return ``True`` if current platform is Windows."""
     return sys.platform.startswith("win32")
 
 
 @cache
 def is_wsl1() -> bool:
-    """Return `True` if current platform is running over Windows Subsystem for Linux v1.
+    """Return ``True`` if current platform is running over Windows Subsystem for Linux v1.
 
     .. caution::
         The only difference between WSL1 and WSL2 is `the case of the kernel release
@@ -552,11 +552,11 @@ def is_wsl1() -> bool:
 
 @cache
 def is_wsl2() -> bool:
-    """Return `True` if current platform is running over Windows Subsystem for Linux v2."""
+    """Return ``True`` if current platform is running over Windows Subsystem for Linux v2."""
     return "microsoft" in platform.release()
 
 
 @cache
 def is_xenserver() -> bool:
-    """Return `True` if current platform is XenServer."""
+    """Return ``True`` if current platform is XenServer."""
     return distro.id() == "xenserver"
