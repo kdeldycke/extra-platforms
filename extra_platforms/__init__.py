27178c5c5282a190b1feade1068081675d7af5a7
kdeldycke <159718+kdeldycke@users.noreply.github.com>
2025-10-21 16:14:49 +0000

    [changelog] Bump minor version to v4.1.0

--- extra_platforms/__init__.py
+++ extra_platforms/__init__.py
@@ -199,7 +199,7 @@ from .operations import (  # noqa: E402
     reduce,
 )
 
-__version__ = "4.0.1"
+__version__ = "4.1.0"
 """Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
