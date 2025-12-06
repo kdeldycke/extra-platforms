73ca01ec56d466cb8630851e83336040750503d4
kdeldycke <kdeldycke@users.noreply.github.com>
2025-12-06 15:14:37 +0000

    [changelog] Post-release version bump

--- extra_platforms/__init__.py
+++ extra_platforms/__init__.py
@@ -204,7 +204,7 @@ from .operations import (  # noqa: E402
     reduce,
 )
 
-__version__ = "5.1.0"
+__version__ = "5.1.1"
 """Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
