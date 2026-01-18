04522cabc520c84b169da9db1d3d964d4fbfecbf
Kevin Deldycke <kevin@deldycke.com>
2026-01-18 16:36:57 +0400

    Make role MyST syntax explicit

--- tests/test_sphinx_crossrefs.py
+++ tests/test_sphinx_crossrefs.py
@@ -35,188 +35,161 @@ MODULES_TO_STUB = [
 
 
 @pytest.mark.parametrize(
-    ("role_type", "reference", "expected_href", "expected_text"),
+    ("role", "expected_href", "expected_text"),
     [
         pytest.param(
-            "func",
-            "~extra_platforms.current_traits",
+            "{func}`~extra_platforms.current_traits`",
             "detection.html#extra_platforms.current_traits",
             "current_traits()",
             id="func_current_traits_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.current_architecture",
+            "{func}`~extra_platforms.current_architecture`",
             "detection.html#extra_platforms.current_architecture",
             "current_architecture()",
             id="func_current_architecture_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.current_platform",
+            "{func}`~extra_platforms.current_platform`",
             "detection.html#extra_platforms.current_platform",
             "current_platform()",
             id="func_current_platform_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.current_ci",
+            "{func}`~extra_platforms.current_ci`",
             "detection.html#extra_platforms.current_ci",
             "current_ci()",
             id="func_current_ci_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.is_aarch64",
+            "{func}`~extra_platforms.is_aarch64`",
             "detection.html#extra_platforms.is_aarch64",
             "is_aarch64()",
             id="func_is_aarch64_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.is_linux",
+            "{func}`~extra_platforms.is_linux`",
             "detection.html#extra_platforms.is_linux",
             "is_linux()",
             id="func_is_linux_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.is_macos",
+            "{func}`~extra_platforms.is_macos`",
             "detection.html#extra_platforms.is_macos",
             "is_macos()",
             id="func_is_macos_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.is_windows",
+            "{func}`~extra_platforms.is_windows`",
             "detection.html#extra_platforms.is_windows",
             "is_windows()",
             id="func_is_windows_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.invalidate_caches",
+            "{func}`~extra_platforms.invalidate_caches`",
             "detection.html#extra_platforms.invalidate_caches",
             "invalidate_caches()",
             id="func_invalidate_caches_in_detection",
         ),
         pytest.param(
-            "func",
-            "~extra_platforms.reduce",
+            "{func}`~extra_platforms.reduce`",
             "operations.html#extra_platforms.reduce",
             "reduce()",
             id="func_reduce_in_operations",
         ),
         pytest.param(
-            "class",
-            "~extra_platforms.Trait",
+            "{class}`~extra_platforms.Trait`",
             "trait.html#extra_platforms.Trait",
             "Trait",
             id="class_trait_in_trait",
         ),
         pytest.param(
-            "class",
-            "~extra_platforms.Platform",
+            "{class}`~extra_platforms.Platform`",
             "trait.html#extra_platforms.Platform",
             "Platform",
             id="class_platform_in_trait",
         ),
         pytest.param(
-            "class",
-            "~extra_platforms.Architecture",
+            "{class}`~extra_platforms.Architecture`",
             "trait.html#extra_platforms.Architecture",
             "Architecture",
             id="class_architecture_in_trait",
         ),
         pytest.param(
-            "class",
-            "~extra_platforms.CI",
+            "{class}`~extra_platforms.CI`",
             "trait.html#extra_platforms.CI",
             "CI",
             id="class_ci_in_trait",
         ),
         pytest.param(
-            "class",
-            "~extra_platforms.Group",
+            "{class}`~extra_platforms.Group`",
             "group.html#extra_platforms.Group",
             "Group",
             id="class_group_in_group",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.AARCH64",
+            "{data}`~extra_platforms.AARCH64`",
             "architecture_data.html#extra_platforms.AARCH64",
             "AARCH64",
             id="data_aarch64_in_architecture_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.X86_64",
+            "{data}`~extra_platforms.X86_64`",
             "architecture_data.html#extra_platforms.X86_64",
             "X86_64",
             id="data_x86_64_in_architecture_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.MACOS",
+            "{data}`~extra_platforms.MACOS`",
             "platform_data.html#extra_platforms.MACOS",
             "MACOS",
             id="data_macos_in_platform_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.UBUNTU",
+            "{data}`~extra_platforms.UBUNTU`",
             "platform_data.html#extra_platforms.UBUNTU",
             "UBUNTU",
             id="data_ubuntu_in_platform_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.WINDOWS",
+            "{data}`~extra_platforms.WINDOWS`",
             "platform_data.html#extra_platforms.WINDOWS",
             "WINDOWS",
             id="data_windows_in_platform_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.GITHUB_CI",
+            "{data}`~extra_platforms.GITHUB_CI`",
             "ci_data.html#extra_platforms.GITHUB_CI",
             "GITHUB_CI",
             id="data_github_ci_in_ci_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.GITLAB_CI",
+            "{data}`~extra_platforms.GITLAB_CI`",
             "ci_data.html#extra_platforms.GITLAB_CI",
             "GITLAB_CI",
             id="data_gitlab_ci_in_ci_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.ALL_PLATFORMS",
+            "{data}`~extra_platforms.ALL_PLATFORMS`",
             "group_data.html#extra_platforms.ALL_PLATFORMS",
             "ALL_PLATFORMS",
             id="data_all_platforms_in_group_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.LINUX",
+            "{data}`~extra_platforms.LINUX`",
             "group_data.html#extra_platforms.LINUX",
             "LINUX",
             id="data_linux_in_group_data",
         ),
         pytest.param(
-            "data",
-            "~extra_platforms.UNIX",
+            "{data}`~extra_platforms.UNIX`",
             "group_data.html#extra_platforms.UNIX",
             "UNIX",
             id="data_unix_in_group_data",
         ),
     ],
 )
-def test_crossref_links_to_correct_page(
-    sphinx_app, role_type, reference, expected_href, expected_text
-):
+def test_crossref_links_to_correct_page(sphinx_app, role, expected_href, expected_text):
     """Test that Sphinx cross-references link to the correct module page.
 
     This test ensures that symbols exposed in the root extra_platforms module
@@ -231,7 +204,7 @@ def test_crossref_links_to_correct_page(
     content = dedent(f"""
         # Test Document
 
-        Here is a reference: {{{role_type}}}`{reference}`
+        Here is a reference: {role}
     """)
 
     html_output = sphinx_app.build_document(content)
